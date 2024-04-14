9762832d10d0a7a9124711831d901cad6a2adc24
dejan-singular <151757983+dejan-singular@users.noreply.github.com>
2024-04-14 09:33:19 +0200

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@
     </div>
     <div class="flex grid-x-2 border-t border-neutral-200 dark:border-neutral-750 px-3 py-2.5 lg:px-4 lg:py-3">
       <User/>
-      /*-- <About/> */
+     
     </div>
   </div>
 </template>
